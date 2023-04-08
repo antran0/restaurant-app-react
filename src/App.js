8f31054bd48ecc177070acfb8b17ff0a499567ce
Steven Tran <steven.tran550@gmail.com>
2023-04-07 19:34:51 -0700

    added restaurant descrption

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import MainHeader from "./components/MainHeader/MainHeader";
+import MainContent from "./components/MainContent/MainContent";
 
 function App() {
   return (
     <React.Fragment>
       <MainHeader />
+      <MainContent />
     </React.Fragment>
   );
 }
