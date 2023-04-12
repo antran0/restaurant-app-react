e75b0f1ac95a8fe2a11c59d16a37fe417e6b6c87
Steven Tran <steven.tran550@gmail.com>
2023-04-11 22:38:54 -0700

    stub components for displaying items placed in cart

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./Cart.module.css";
+import Backdrop from "../UI/Backdrop";
+
+const Cart = (props) => {
+  return <React.Fragment></React.Fragment>;
+};
+
+export default Cart;
---
+++ src/components/Cart/CartItems.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./CartItems.module.css";
+
+const CartItems = (props) => {
+  return <ul></ul>;
+};
+
+export default CartItems;
